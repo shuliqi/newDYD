a05868489863347049400d8656e642261893cea4
shuliqi <1169046371@qq.com>
2016-04-04 14:18:36 +0800

    个人主页完成

--- newDiyidan/views/js/app.js
+++ newDiyidan/views/js/app.js
@@ -29,31 +29,50 @@ diyidanApp.config(function($stateProvider, $urlRouterProvider) {
                 '': {
                     templateUrl: 'tpls/home.html'
                 },
-                'main@index': {
-                    templateUrl: 'tpls/index.html'
+                'header@index': {
+                    templateUrl: 'tpls/header.html'
+                },
+                'middle@index': {
+                    templateUrl: 'tpls/middle.html'
+                },
+                'footer@index': {
+                    templateUrl: 'tpls/footer.html'
                 }
-                
             }
         })
-        .state('booklist', {
-            url: '/{bookType:[0-9]{1,4}}',
+        .state('personalIndex', {
+            // url: '/{personalIndex:[0-9]{1,4}}',
+            url:'/personalIndex',
             views: { //注意这里的写法，当一个页面上带有多个ui-view的时候如何进行命名和视图模板的加载动作
                 '': {
-                    templateUrl: 'tpls/bookList.html'
+                    templateUrl: 'tpls/personalIndex.html'
+                }
+                // 'booktype@booklist': {
+                //     templateUrl: 'tpls/bookType.html'
+                // },
+                // 'bookgrid@booklist': {
+                //     templateUrl: 'tpls/bookGrid.html'
+                // }
+            }
+        })
+        .state('hostSpot', {
+            url: '/hostSpot',
+            views: {
+                '': {
+                    templateUrl: 'tpls/hostSpot.html'
                 },
-                'booktype@booklist': {
-                    templateUrl: 'tpls/bookType.html'
+                'header@hostSpot': {
+                    templateUrl: 'tpls/header.html'
                 },
-                'bookgrid@booklist': {
-                    templateUrl: 'tpls/bookGrid.html'
+                'host@hostSpot': {
+                    templateUrl: 'tpls/host.html'
+                },
+                'footer@hostSpot': {
+                    templateUrl: 'tpls/footer.html'
                 }
             }
         })
-        .state('addbook', {
-            url: '/addbook',
-            templateUrl: 'tpls/addBookForm.html'
-        })
-        .state('bookdetail', {
+        .state('个人主页', {
             url: '/bookdetail/:bookId', //注意这里在路由中传参数的方式
             templateUrl: 'tpls/bookDetail.html'
         })
