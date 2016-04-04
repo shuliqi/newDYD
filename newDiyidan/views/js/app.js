4f1c9256c5c9f3c8351d4a3af52b5ba9d349b5b4
shuliqi <1169046371@qq.com>
2016-04-04 21:19:26 +0800

    个人主页修改完成

--- newDiyidan/views/js/app.js
+++ newDiyidan/views/js/app.js
@@ -46,13 +46,13 @@ diyidanApp.config(function($stateProvider, $urlRouterProvider) {
             views: { //注意这里的写法，当一个页面上带有多个ui-view的时候如何进行命名和视图模板的加载动作
                 '': {
                     templateUrl: 'tpls/personalIndex.html'
+                },
+                'my_mlddle@personalIndex': {
+                    templateUrl: 'tpls/my_middle.html'
+                },
+                'footer@personalIndex': {
+                    templateUrl: 'tpls/footer.html'
                 }
-                // 'booktype@booklist': {
-                //     templateUrl: 'tpls/bookType.html'
-                // },
-                // 'bookgrid@booklist': {
-                //     templateUrl: 'tpls/bookGrid.html'
-                // }
             }
         })
         .state('hostSpot', {
